365df5f49e16332649706b93777fa3dc7abc5dbc
MidnightShad0w <samsonchik2406@mail.ru>
2023-09-28 23:12:45 +0300

    unique email added & minor changes

--- src/main/java/com/danila/javafxauth/controllers/LoginController.java
+++ src/main/java/com/danila/javafxauth/controllers/LoginController.java
@@ -3,22 +3,17 @@ package com.danila.javafxauth.controllers;
 import com.danila.javafxauth.Main;
 import com.danila.javafxauth.database.DatabaseConnection;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.sql.*;
 
 public class LoginController {
 
     @FXML
-    private TextField usernameField;
+    private TextField emailField;
 
     @FXML
     private PasswordField passwordField;
@@ -34,31 +29,35 @@ public class LoginController {
 
     @FXML
     private void loginButtonAction() {
-        String username = usernameField.getText();
+        String email = emailField.getText();
         String password = passwordField.getText();
         String uuid = Main.getInstance().getUUID();
 
         try {
-            Connection connection = DatabaseConnection.getConnection(); // Получение соединения с базой данных
-            String sql = "SELECT * FROM users WHERE name = ? AND password = ? AND uuid = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, username);
+            Connection connection = DatabaseConnection.getConnection();
+            String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
             preparedStatement.setString(2, password);
-            preparedStatement.setString(3, uuid);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                messageLabel.setText("Вход выполнен успешно");
-                Main.getInstance().switchToSuccessPage();
+                String storedUUID = resultSet.getString("uuid");
+                if (storedUUID.equals(uuid)) {
+                    messageLabel.setText("Вход выполнен успешно");
+                    Main.getInstance().switchToSuccessPage();
+                } else {
+                    messageLabel.setText("Доступ запрещен");
+                    Main.getInstance().switchToAccessDeniedPage();
+                }
             } else {
                 messageLabel.setText("Неверный логин или пароль");
-                Main.getInstance().switchToAccessDeniedPage();
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 
     @FXML
     private void registrationButtonAction() {
--- src/main/java/com/danila/javafxauth/controllers/RegistrationController.java
+++ src/main/java/com/danila/javafxauth/controllers/RegistrationController.java
@@ -10,6 +10,7 @@ import javafx.util.Duration;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RegistrationController {
@@ -29,22 +30,39 @@ public class RegistrationController {
     @FXML
     private Button registrationButton;
 
-    //TODO unique email check
     //TODO dokerize db and app
 
+    // Метод создания всплывающей подсказки
     private void showTooltip(Control control, String message, int durationMillis) {
         Tooltip tooltip = new Tooltip(message);
         tooltip.setShowDuration(Duration.millis(durationMillis));
 
-        // Устанавливаем позицию AnchorLocation для Tooltip
         tooltip.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
 
         Tooltip.install(control, tooltip);
         tooltip.show(control.getScene().getWindow());
     }
 
+    // Метод очистки всплывающей подсказки
     private void clearTooltip(Control control) {
         Tooltip.uninstall(control, null);
+    }
+
+    private boolean isEmailUnique(String email) {
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            String query = "SELECT COUNT(*) FROM users WHERE email = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+            preparedStatement.close();
+            return count == 0; // Если count равен 0, email уникален
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Если произошла ошибка считаем email неуникальным
+        }
     }
 
     // Метод для обработки нажатия кнопки "Зарегистрироваться"
@@ -61,6 +79,12 @@ public class RegistrationController {
         alert.setHeaderText(null);
 
 
+        if (uuid == null) {
+            alert.setTitle("Ошибка UUID");
+            alert.setContentText("Не удалось получить UUID устройства");
+            return;
+        }
+
         // Проверка ввода имени
         if (name.isEmpty()) {
             showTooltip(nameField, "Введите имя", 1000);
@@ -71,7 +95,7 @@ public class RegistrationController {
 
         // Проверка ввода телефона (допустимы только цифры)
         if (phone.isEmpty() || !phone.matches("\\d+")) {
-            showTooltip(phoneField, "Номер телефона должен содержать только цифры.", 100);
+            showTooltip(phoneField, "Номер телефона должен содержать только цифры", 100);
             return;
         } else {
             clearTooltip(phoneField);
@@ -79,7 +103,7 @@ public class RegistrationController {
 
         // Проверка ввода email (валидная почта)
         if (email.isEmpty() || !email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")) {
-            showTooltip(emailField, "Пожалуйста, введите корректный адрес электронной почты.", 1000);
+            showTooltip(emailField, "Пожалуйста, введите корректный адрес электронной почты", 1000);
             return;
         } else {
             clearTooltip(emailField);
@@ -87,19 +111,29 @@ public class RegistrationController {
 
         // Проверка ввода пароля (буквы и цифры, минимум 6 знаков)
         if (password.isEmpty() || !password.matches("^[A-Za-z0-9]{6,}$")) {
-            showTooltip(passwordField, "Пароль должен содержать буквы и цифры и быть не менее 6 символов.", 1000);
+            showTooltip(passwordField, "Пароль должен содержать буквы и цифры и быть не менее 6 символов", 1000);
             return;
         } else {
             clearTooltip(passwordField);
         }
 
+        // Проверка на уникальность логина
+        if (!isEmailUnique(email)) {
+//            alert.setTitle("Неверный email"); // хз поч не появляется alert
+//            alert.setContentText("Пользователь с таким email уже существует");
+//            return;
+            showTooltip(emailField, "Такой email уже существует", 1000);
+            return;
+        } else {
+            clearTooltip(emailField);
+        }
 
         try {
             // Создаем подключение к бд
             Connection connection = DatabaseConnection.getConnection();
             // SQL-запрос для вставки данных в таблицу
-            String insertQuery = "INSERT INTO users (name, phone, email, password, uuid) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            String query = "INSERT INTO users (name, phone, email, password, uuid) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, phone);
             preparedStatement.setString(3, email);
@@ -120,7 +154,7 @@ public class RegistrationController {
             }
 
             preparedStatement.close();
-//            connection.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
             alert.setTitle("Ошибка SQL");
